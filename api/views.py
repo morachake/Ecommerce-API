031af43cb0209f1cdc4b87ebb8fe6777e373529e
morachake <jacobmoracha@gmail.com>
2025-01-17 18:03:49 +0300

    add product list view and serializer for Product model

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
+from django.http import JsonResponse
+from  api.serializers import ProductSerializer
+from api.models import Product, Order, OrderItem
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return JsonResponse(serializer.data, safe=False)
